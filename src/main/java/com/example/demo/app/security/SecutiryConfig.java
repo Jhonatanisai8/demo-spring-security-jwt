6e7641bf338773fdd1f71185ededfe34195effe6
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-20 18:25:44 -0500

    configuracion de Spring Security

---
+++ src/main/java/com/example/demo/app/security/SecutiryConfig.java
@@ -0,0 +1,64 @@
+package com.example.demo.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecutiryConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests.requestMatchers("/hello").permitAll();
+                    authorizeRequests.anyRequest().authenticated();
+                })
+                .sessionManagement(sesion -> {
+                    sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    //detalles de un usuario
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("jhos")
+                .password("mnbvcxz")
+                .roles()
+                .build());
+        return manager;
+    }
+
+    //codificacion de la contraseña
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    //autenticacion de los usuarios
+    @Bean
+    public AuthenticationManager authenticationManager(
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(authenticationProvider);
+    }
+
+
+}
